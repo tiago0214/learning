301e785bf0a53a2ffb4148d02a172ddcb2d71e0d
tiago <tiago0214@gmail.com>
2023-04-14 06:50:20 -0300

    mais um pouco

--- objeto/criandoObjetos.js
+++ objeto/criandoObjetos.js
@@ -28,3 +28,32 @@ const prod2 = new produto ('notebook',2999.99,0.25)
 console.log(prod1)
 console.log(prod1.getPrecoComDesconto())
 console.log(prod2.getPrecoComDesconto())
+
+//criando objeto: função factory
+
+function func (nome,salarioBase,faltas){
+    return { //return {}... eu criei um objeto com notação literal
+        nome,
+        salarioBase,
+        faltas,
+        getSalario (){
+            return (salarioBase/30) * (30-faltas)
+        }
+    }
+}
+
+const f1 = func('Tiago',1600,1)
+
+console.log(f1.getSalario())
+
+//Object.create
+
+const filha = Object.create(null)
+filha.nome ='Anna'
+console.log(filha)
+
+//Um função famosa que retorna Objeto.. Ela transforma um JSON em um objeto... Ela não transforma em texto, mas sim em OBJETO
+//JSON é em string... parece com objeto porque tem {}, mas é diferente.
+console.log('---------------------------------')
+const fromJSON = JSON.parse('{"info":"Sou um JSON"}')
+console.log(fromJSON.info)
