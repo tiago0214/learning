2698c041eecbdfe99505ae8ecc5d73783dc4db28
Tiago Souza <tiago0214@gmail.com>
2023-05-01 21:57:19 -0300

    gh

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,11 @@
-alert('Ola')
+function calcular(){
+    let numero = Number(document.getElementById('num').value)
+    let res = document.getElementById('res')
+    res.innerHTML=''
+    for(c = 0;c<=10;c++){
+        let item = document.createElement('option')
+        item.value=`res${c}`
+        item.text= `${numero} * ${c} = ${numero*c}`
+        res.appendChild(item)
+    }
+}
